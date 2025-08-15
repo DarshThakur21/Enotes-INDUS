3715ee4bd4e36e67f2a51c79d80fdb421fe96619
Darsh Thakur <thakurdarsh21@gmail.com>
2025-08-16 03:16:34 +0530

    first initial commit

---
+++ src/main/java/com/enotes/Enotes_INDUS/EnotesIndusApplication.java
@@ -0,0 +1,13 @@
+package com.enotes.Enotes_INDUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesIndusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesIndusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enotes/Enotes_INDUS/EnotesIndusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enotes.Enotes_INDUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesIndusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
