7d075235fc64d05a3d4fc8a03bd450830be9d09a
DarshThakur21 <thakurdarsh21@gmail.com>
2025-12-05 12:30:05 +0530

    todo loggings

--- src/main/java/com/enotes/Enotes_INDUS/controller/TodoController.java
+++ src/main/java/com/enotes/Enotes_INDUS/controller/TodoController.java
@@ -29,16 +29,19 @@ public class TodoController
             @PreAuthorize("hasRole('USER')")
             public ResponseEntity<?> getAllTodos(){
                 try {
-
+                log.info("TodoController : getAllTodos() : Start");
                 int userId=CommonUtil.getLoggedInUser().getId();
                 List<TodoDto> todoDtoList =todoService.getTodoByUser();
                     if (CollectionUtils.isEmpty(todoDtoList)){
                         return ResponseEntity.noContent().build();
                     }
+                    log.info("List Found");
+                    log.info("TodoController : getAllTodos() : End");
                 return CommonUtil.createBuildResponse(todoDtoList, HttpStatus.OK);
 
                 } catch (Exception e) {
                     e.printStackTrace();
+                    log.error("List Not found");
                     return CommonUtil.createErrorResponseMessage("list not found",HttpStatus.NOT_FOUND);
                 }
 
@@ -48,17 +51,21 @@ public class TodoController
             @PreAuthorize("hasRole('USER')")
             public ResponseEntity<?> saveTodos(@RequestBody  TodoDto todoDto){
                 try {
-
+                    log.info("TodoController : saveTodos() : Start");
                 Boolean saved=todoService.saveTodo(todoDto);
                 if (!saved){
+                log.info("Cannot save the todo");
                     return CommonUtil.createErrorResponseMessage("cannnot save ",HttpStatus.NOT_ACCEPTABLE);
 
                 }
-
+                log.info("Save success");
+                log.info("TodoController : saveTodos() : End");
                 return CommonUtil.createBuildResponseMessage("saved success", HttpStatus.CREATED);
 
                 } catch (Exception e) {
+
                     e.printStackTrace();
+                    log.error("Error saving todo");
                     throw new RuntimeException(e);
 
                 }
@@ -68,45 +75,47 @@ public class TodoController
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<?> getTodoById(@PathVariable Integer id){
         try {
+            log.info("TodoController : getTodoById() : Start");
                 TodoDto todoDto=todoService.getTodoById(id);
-                if(!ObjectUtils.isEmpty(todoDto)){
-                    return CommonUtil.createBuildResponse(todoDto, HttpStatus.OK);
-
+                if(ObjectUtils.isEmpty(todoDto)){
+                log.info("Todo list not found");
+                    return CommonUtil.createErrorResponseMessage("Not found todo", HttpStatus.NOT_FOUND);
                 }
-                return CommonUtil.createErrorResponseMessage("Not found todo", HttpStatus.NOT_FOUND);
+                log.info("Todo list found");
+                log.info("TodoController : getTodoById() : End");
 
+                    return CommonUtil.createBuildResponse(todoDto, HttpStatus.OK);
 
 
 
         } catch (Exception e) {
+            log.error("Todo list not found");
             e.printStackTrace();
             throw new RuntimeException(e);
-
-
         }
-
     }
 
     @GetMapping("/by-status/{status}")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<?> getTodoById(@PathVariable String status){
+    public ResponseEntity<?> getTodoByStatus(@PathVariable String status){
         try {
+            log.info("TodoController : getTodoByStatus() : Start");
+
             List<TodoDto> todoDto=todoService.getByStatus(status);
-            if(!ObjectUtils.isEmpty(todoDto)){
-                return CommonUtil.createBuildResponse(todoDto, HttpStatus.OK);
+            if(ObjectUtils.isEmpty(todoDto)){
+            log.info("Not found todo with the status");
+                return CommonUtil.createErrorResponseMessage("Not found todo with this status", HttpStatus.NOT_FOUND);
 
             }
-            return CommonUtil.createErrorResponseMessage("Not found todo with this status", HttpStatus.NOT_FOUND);
-
-
-
+            log.info("Todo with the status:{}",status);
+            log.info("TodoController : getTodoByStatus() : End");
+            return CommonUtil.createBuildResponse(todoDto, HttpStatus.OK);
 
         } catch (Exception e) {
+
+            log.error("Todo list not found");
             e.printStackTrace();
             throw new RuntimeException(e);
-
-
         }
-
     }
 }
