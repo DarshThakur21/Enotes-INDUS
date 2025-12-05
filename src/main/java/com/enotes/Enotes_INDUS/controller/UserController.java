2db59808bd395f2a3154b175c611c506af5576da
DarshThakur21 <thakurdarsh21@gmail.com>
2025-12-05 12:48:38 +0530

    user logging

--- src/main/java/com/enotes/Enotes_INDUS/controller/UserController.java
+++ src/main/java/com/enotes/Enotes_INDUS/controller/UserController.java
@@ -5,12 +5,15 @@ import com.enotes.Enotes_INDUS.dto.UserResponseDto;
 import com.enotes.Enotes_INDUS.model.User;
 import com.enotes.Enotes_INDUS.service.UserService;
 import com.enotes.Enotes_INDUS.utils.CommonUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -22,23 +25,26 @@ public class UserController {
     private ModelMapper modelMapper;
 
     @GetMapping("/user-profiles")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAllUserProfiles(){
+        log.info("UserController : getAllUserProfiles() : Start ");
         User loggedInUser= CommonUtil.getLoggedInUser();
         UserResponseDto userResponseDto=modelMapper.map(loggedInUser, UserResponseDto.class);
+        log.info("UserController : getAllUserProfiles() : End ");
         return CommonUtil.createBuildResponse(userResponseDto, HttpStatus.OK);
 
     }
 
 
     @PostMapping("/change-password")
-    public ResponseEntity<?> chagnePassword(@RequestBody PasswordChangeRequest passwordChangeRequest){
-//        User loggedInUser= CommonUtil.getLoggedInUser();
-
+    public ResponseEntity<?> changePassword(@RequestBody PasswordChangeRequest passwordChangeRequest){
+        log.info("UserController : changePassword() : Start ");
         if(userService.changePassword(passwordChangeRequest)){
 
-        return CommonUtil.createBuildResponseMessage("Password Changed successfully", HttpStatus.OK);
+            log.info("UserController : changePassword() : End");
+            return CommonUtil.createBuildResponseMessage("Password Changed successfully", HttpStatus.OK);
         }
-
+        log.info("UserController : changePassword() : End");
         return CommonUtil.createErrorResponseMessage("Password Couldnt be change please try again later", HttpStatus.FORBIDDEN);
 
 
