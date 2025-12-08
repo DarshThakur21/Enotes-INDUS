4fddda7370b99b5d98427f85e31883f15778cb0d
DarshThakur21 <thakurdarsh21@gmail.com>
2025-12-08 18:10:30 +0530

    swagger impl

---
+++ src/main/java/com/enotes/Enotes_INDUS/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.enotes.Enotes_INDUS.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        OpenAPI openApi=new OpenAPI();
+        Info info=new Info();
+        info.setTitle("ENOTES INDUS APIS");
+        info.setDescription("LIST OF ALL THE API CALLS PRESENT IN THE ENOTES APP");
+        info.setVersion("1.0.0");
+        info.setTermsOfService("http://enotes.com");
+        info.setContact(
+                new Contact().email("chaseaccel2@gmail.com").name("Chase").url("http://enotes.com")
+        );
+        info.setLicense(new License().name("Enotes 1.1").url("http://enotes.com"));
+        List<Server> serversList =List.of(new Server().description("DEV").url("http://localhost:8085/enotes"),
+        new Server().description("TEST").url("http://localhost:8085/enotes"),
+        new Server().description("PROD").url("http://localhost:8085/enotes"));
+        SecurityScheme securityScheme=new SecurityScheme().name("Authorization")
+                        .scheme("bearer").type(SecurityScheme.Type.HTTP)
+                        .bearerFormat("JWT").in(SecurityScheme.In.HEADER);
+
+        Components components=new Components();
+        components.addSecuritySchemes("JwtToken",securityScheme);
+        openApi.setComponents(components);
+        openApi.setInfo(info);
+        openApi.setServers(serversList);
+        openApi.setSecurity(List.of(new SecurityRequirement().addList("JwtToken")));
+        return openApi;
+    }
+}
