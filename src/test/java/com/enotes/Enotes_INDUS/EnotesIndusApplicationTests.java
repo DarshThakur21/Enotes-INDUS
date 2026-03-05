8f5b14dda6037c90300bd000c6739722c7d32fae
Darsh Thakur <thakurdarsh21@gmail.com>
2026-03-06 00:34:22 +0530

    workflow tryout

--- src/test/java/com/enotes/Enotes_INDUS/EnotesIndusApplicationTests.java
+++ src/test/java/com/enotes/Enotes_INDUS/EnotesIndusApplicationTests.java
@@ -1,9 +1,11 @@
 package com.enotes.Enotes_INDUS;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class EnotesIndusApplicationTests {
 
 	@Test
